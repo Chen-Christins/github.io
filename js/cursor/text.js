9c150beaaba567ea1a8d37cc1f754aac75fff844
17354303956 <17354303956@163.com>
2024-02-14 08:08:07 +0800

    Site updated: 2024-02-14 08:08:06

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -1,10 +1,10 @@
 var a_idx = 0;
-jQuery(document).ready(function($) {
-  $("body").click(function(e) {
+jQuery(document).ready(function ($) {
+  $("body").click(function (e) {
     var a = new Array("喜欢我", "不喜欢我");
     var $i = $("<span/>").text(a[a_idx]);
     var x = e.pageX,
-    y = e.pageY;
+      y = e.pageY;
     $i.css({
       "z-index": 99999,
       "top": y - 28,
@@ -16,7 +16,7 @@ jQuery(document).ready(function($) {
     $i.animate({
       "top": y - 180,
       "opacity": 0
-    }, 1500, function() {
+    }, 1500, function () {
       $i.remove();
     });
     a_idx = (a_idx + 1) % a.length;
