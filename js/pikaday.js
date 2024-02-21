9058fa7b6bb57f348cd4d5606d63f2d10560adff
17354303956 <17354303956@163.com>
2024-02-21 15:40:16 +0800

    Site updated: 2024-02-21 15:40:15

--- js/pikaday.js
+++ js/pikaday.js
@@ -22,7 +22,7 @@
       return [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
     },
     Pikaday = function (opts) {
-      var data = '{"2023":{"11":{"21":true,"22":true,"24":true,"29":true,"31":true}},"2024":{"0":{"3":true,"5":true,"10":true,"14":true,"16":true,"19":true,"21":true,"26":true,"27":true,"28":true,"29":true,"31":true},"1":{"2":true,"4":true,"5":true,"6":true,"7":true,"9":true,"10":true,"11":true,"12":true,"14":true,"16":true}}}';
+      var data = '{"2023":{"11":{"21":true,"22":true,"24":true,"29":true,"31":true}},"2024":{"0":{"3":true,"5":true,"10":true,"14":true,"16":true,"19":true,"21":true,"26":true,"27":true,"28":true,"29":true,"31":true},"1":{"2":true,"4":true,"5":true,"6":true,"7":true,"9":true,"10":true,"11":true,"12":true,"14":true,"16":true,"21":true}}}';
       this.data = JSON.parse(data);
       window.pdata = this.data;
       this.weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
